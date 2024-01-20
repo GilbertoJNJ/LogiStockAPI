ae4923d9740fc790aff4aff90d7aa5ddf8a88e4e
Gilberto Júnior <gilbertojnj@gmail.com>
2024-01-20 16:52:33 -0300

    Refatora service

--- src/main/java/com/gilberto/logistockapi/services/ProductService.java
+++ src/main/java/com/gilberto/logistockapi/services/ProductService.java
@@ -1,18 +1,21 @@
 package com.gilberto.logistockapi.services;
 
+import com.gilberto.logistockapi.mappers.IProductMapper;
+import com.gilberto.logistockapi.models.dto.request.ProductBaseForm;
 import com.gilberto.logistockapi.models.dto.request.ProductForm;
-import com.gilberto.logistockapi.models.dto.response.MessageResponseDTO;
+import com.gilberto.logistockapi.models.dto.request.QuantityForm;
+import com.gilberto.logistockapi.models.dto.response.ProductDTO;
 import com.gilberto.logistockapi.models.entity.Product;
 import com.gilberto.logistockapi.exceptions.ProductAlreadyRegisteredException;
 import com.gilberto.logistockapi.exceptions.ProductNotFoundException;
 import com.gilberto.logistockapi.exceptions.ProductStockExceededException;
 import com.gilberto.logistockapi.exceptions.ProductStockUnderThanZeroException;
 import com.gilberto.logistockapi.repositories.ProductRepository;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,86 +23,85 @@ public class ProductService implements IProductService {
 
     private final ProductRepository productRepository;
     
-    public ProductService(@Autowired ProductRepository productRepository) {
+    private final IProductMapper productMapper;
+    
+    public ProductService(@Autowired ProductRepository productRepository,
+                          @Autowired IProductMapper productMapper) {
         this.productRepository = productRepository;
+        this.productMapper     = productMapper;
     }
 
     @Override
-    public MessageResponseDTO createProduct(ProductForm productForm) throws ProductAlreadyRegisteredException{
+    public ProductDTO create(ProductForm productForm) throws ProductAlreadyRegisteredException{
 
         verifyIfIsAlreadyRegistered(productForm.getBarCode());
         
-        var productToSave = new Product(productForm);
-        var savedProduct = this.productRepository.save(productToSave);
-        return createMessageResponse(savedProduct.getId(), "Created product with ID " );
+        var product = this.productMapper.toProduct(productForm);
+        var savedProduct = this.productRepository.save(product);
+        return this.productMapper.toProductDTO(savedProduct);
     }
     
     @Override
-    public List<ProductForm> listAll() {
-        List<Product> allProduct = productRepository.findAll();
-        return allProduct.stream()
-                .map(ProductForm::new)
-                .collect(Collectors.toList());
+    public List<ProductDTO> listAll() {
+        return this.productRepository.findAll().stream()
+            .map(this.productMapper::toProductDTO)
+            .collect(Collectors.toList());
     }
     
     @Override
-    public ProductForm findByBarCode(String barCode) throws ProductNotFoundException {
+    public ProductDTO findByBarCode(String barCode) throws ProductNotFoundException {
         return productRepository.findByBarCode(barCode)
-            .map(ProductForm::new)
+            .map(this.productMapper::toProductDTO)
             .orElseThrow(() -> new ProductNotFoundException(barCode));
     }
     
     @Override
-    public MessageResponseDTO delete(Long id) throws ProductNotFoundException {
+    public void delete(Long id) throws ProductNotFoundException {
         this.verifyIfExists(id);
         this.productRepository.deleteById(id);
-        return createMessageResponse(id, "Deleted product with ID ");
     }
     
     @Override
-    public MessageResponseDTO updateById(Long id, ProductForm productForm) throws ProductNotFoundException {
-        var productToUpdate = this.verifyIfExists(id);
-        productToUpdate.setName(productForm.getName());
-        productToUpdate.setCategory(productForm.getCategory());
-        productToUpdate.setUnitPrice(productForm.getUnitPrice());
-        productToUpdate.setMeasureUnit(productForm.getMeasureUnit());
-        productToUpdate.setStockQuantity(productForm.getStockQuantity());
-        productToUpdate.setMinStockLevel(productForm.getMinStockLevel());
-        productToUpdate.setMaxStockLevel(productForm.getMaxStockLevel());
-        productToUpdate.setDescription(productForm.getDescription());
+    public ProductDTO updateById(Long id, ProductBaseForm productForm) throws ProductNotFoundException {
+        var product = this.verifyIfExists(id);
+        product.setName(productForm.getName());
+        product.setCategory(productForm.getCategory());
+        product.setUnitPrice(productForm.getUnitPrice());
+        product.setMeasureUnit(productForm.getMeasureUnit());
+        product.setDescription(productForm.getDescription());
 
-        var updatedProduct = productRepository.save(productToUpdate);
-        return createMessageResponse(updatedProduct.getId(), "Updated product with ID ");
+        var updatedProduct = productRepository.save(product);
+        
+        return this.productMapper.toProductDTO(updatedProduct);
     }
     
     @Override
-    public ProductForm increment(Long id, Integer quantityToIncrement) throws ProductNotFoundException, ProductStockExceededException {
-        var productToIncrementStock = verifyIfExists(id);
-        var updatedQuantity = sumQuantity(productToIncrementStock.getStockQuantity(),
-            quantityToIncrement);
+    public ProductDTO increment(Long id, @Valid QuantityForm quantity) throws ProductNotFoundException, ProductStockExceededException {
+        var product = verifyIfExists(id);
+        var updatedQuantity = sumQuantity(product.getStockQuantity(), quantity.getQuantity());
         
-        if (updatedQuantity > productToIncrementStock.getMaxStockLevel()) {
-            throw new ProductStockExceededException(id, quantityToIncrement);
+        if (updatedQuantity > product.getMaxStockLevel()) {
+            throw new ProductStockExceededException(id, quantity.getQuantity());
         }
         
-        productToIncrementStock.setStockQuantity(updatedQuantity);
-        var incrementedProductStock = productRepository.save(productToIncrementStock);
-        return new ProductForm(incrementedProductStock);
+        product.setStockQuantity(updatedQuantity);
+        var updatedProduct = productRepository.save(product);
+        return this.productMapper.toProductDTO(updatedProduct);
     }
     
     @Override
-    public ProductForm decrement(Long id, Integer quantityToDecrement) throws ProductNotFoundException, ProductStockUnderThanZeroException {
-        var productToDecrementStock = verifyIfExists(id);
+    public ProductDTO decrement(Long id, @Valid QuantityForm quantity) throws ProductNotFoundException, ProductStockUnderThanZeroException {
+        var product = verifyIfExists(id);
         var quantityAfterDecrement = Math.max(0,
-            subtractQuantity(productToDecrementStock.getStockQuantity(), quantityToDecrement));
+            subtractQuantity(product.getStockQuantity(), quantity.getQuantity()));
         
         if (quantityAfterDecrement == 0) {
-            throw new ProductStockUnderThanZeroException(id, quantityToDecrement);
+            throw new ProductStockUnderThanZeroException(id, quantity.getQuantity());
         }
         
-        productToDecrementStock.setStockQuantity(quantityAfterDecrement);
-        var decrementedProductStock = productRepository.save(productToDecrementStock);
-        return new ProductForm(decrementedProductStock);
+        product.setStockQuantity(quantityAfterDecrement);
+        var updatedProduct = productRepository.save(product);
+        return this.productMapper.toProductDTO(updatedProduct);
     }
     
     private Integer subtractQuantity(Integer stockQuantity, Integer quantityToDecrement) {
@@ -115,17 +117,10 @@ public class ProductService implements IProductService {
                 .orElseThrow(() -> new ProductNotFoundException(id));
     }
     private void verifyIfIsAlreadyRegistered(String barCode) throws ProductAlreadyRegisteredException {
-        Optional<Product> optSavedProduct = this.productRepository.findByBarCode(barCode);
+        var optSavedProduct = this.productRepository.findByBarCode(barCode);
         if (optSavedProduct.isPresent()) {
             throw new ProductAlreadyRegisteredException(barCode);
         }
-    }
-
-    private MessageResponseDTO createMessageResponse(Long id, String message) {
-        return MessageResponseDTO
-                .builder()
-                .message(message + id)
-                .build();
     }
 
 }
