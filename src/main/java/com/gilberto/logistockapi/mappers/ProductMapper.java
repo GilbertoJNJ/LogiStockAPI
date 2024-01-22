55ae858efd60b4b2369e9e7861410272fa1caa22
Gilberto Júnior <gilbertojnj@gmail.com>
2024-01-22 18:35:11 -0300

    Refatora ProductMapper

--- src/main/java/com/gilberto/logistockapi/mappers/ProductMapper.java
+++ src/main/java/com/gilberto/logistockapi/mappers/ProductMapper.java
@@ -2,33 +2,55 @@ package com.gilberto.logistockapi.mappers;
 
 import com.gilberto.logistockapi.models.dto.request.ProductForm;
 import com.gilberto.logistockapi.models.dto.response.ProductDTO;
+import com.gilberto.logistockapi.models.dto.response.SupplierDTO;
 import com.gilberto.logistockapi.models.entity.Product;
+import com.gilberto.logistockapi.models.entity.Supplier;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProductMapper implements IProductMapper{
+public class ProductMapper implements IProductMapper {
   
   @Override
   public Product toProduct(ProductForm productForm) {
-    var product = new Product();
-    product.setName(productForm.getName());
-    product.setCategory(productForm.getCategory());
-    product.setDescription(productForm.getDescription());
-    product.setUnitPrice(productForm.getUnitPrice());
-    product.setBarCode(productForm.getBarCode());
-    product.setStockQuantity(productForm.getStockQuantity());
-    product.setMeasureUnit(productForm.getMeasureUnit());
-    product.setMinStockLevel(productForm.getMinStockLevel());
-    product.setMaxStockLevel(productForm.getMaxStockLevel());
-    product.setSupplier(null);
-    return product;
+    return Product.builder()
+        .name(productForm.name())
+        .category(productForm.category())
+        .description(productForm.description())
+        .unitPrice(productForm.unitPrice())
+        .barCode(productForm.barCode())
+        .stockQuantity(productForm.stockQuantity())
+        .measureUnit(productForm.measureUnit())
+        .maxStockLevel(productForm.maxStockLevel())
+        .build();
   }
   
   @Override
   public ProductDTO toProductDTO(Product product) {
-    return new ProductDTO(product.getId(), product.getName(), product.getBarCode(),
-        product.getCategory(), null, product.getUnitPrice(), product.getMeasureUnit(),
-        product.getStockQuantity(), product.getDescription());
+    return new ProductDTO(
+        product.getId(),
+        product.getName(),
+        product.getBarCode(),
+        product.getCategory(),
+        createSupplierDTO(product.getSupplier()),
+        product.getUnitPrice(),
+        product.getMeasureUnit(),
+        product.getStockQuantity(),
+        product.getDescription()
+    );
+  }
+  
+  private SupplierDTO createSupplierDTO(Supplier supplier) {
+    if (supplier == null) {
+      return null;
+    }
+    
+    return new SupplierDTO(
+        supplier.getId(),
+        supplier.getName(),
+        supplier.getLegalDocument(),
+        supplier.getEmail(),
+        supplier.getPhone()
+    );
   }
   
 }
