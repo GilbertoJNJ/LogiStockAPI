286901632ec43f5c985a9ccce30d72f135bf4269
Gilberto Júnior <gilbertojnj@gmail.com>
2024-01-22 18:31:02 -0300

    Refatora classes de exceptions

--- src/main/java/com/gilberto/logistockapi/exceptions/ProductNotFoundException.java
+++ src/main/java/com/gilberto/logistockapi/exceptions/ProductNotFoundException.java
@@ -1,14 +1,10 @@
 package com.gilberto.logistockapi.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class ProductNotFoundException extends Exception {
+public final class ProductNotFoundException extends HttpException {
 
-    public ProductNotFoundException(Long id) {
-        super("Product not found with ID " + id);
-    }
-    public ProductNotFoundException(String name) { super(String.format("Product not found with Name " + name));
+    public ProductNotFoundException() {
+        super("Product not found!", HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/gilberto/logistockapi/exceptions/ProductStockExceededException.java
+++ src/main/java/com/gilberto/logistockapi/exceptions/ProductStockExceededException.java
@@ -1,11 +1,10 @@
 package com.gilberto.logistockapi.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class ProductStockExceededException extends Exception{
-    public ProductStockExceededException(Long id, int quantityToIncrement) {
-        super(String.format("Product with %s ID to increment informed exceeds the max stock capacity: %s", id, quantityToIncrement));
+public final class ProductStockExceededException extends HttpException{
+    
+    public ProductStockExceededException() {
+        super("Quantity exceeds maximum stock capacity!", HttpStatus.BAD_REQUEST);
     }
 }
